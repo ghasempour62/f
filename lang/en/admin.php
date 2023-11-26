8c006cba896e12f9d1f684fe6e47b8acd5da2cc3
ghasempour62 <145551494+ghasempour62@users.noreply.github.com>
2023-11-26 07:33:26 +0330

    first commit

---
+++ lang/en/admin.php
@@ -0,0 +1,10 @@
+<?php
+ return array (
+  'Theme color' => 'Theme color',
+  'Primary color' => 'Primary color',
+  'Secondary color' => 'Secondary color',
+  'Administrator Status' => 'Administrator Status',
+  'Awaiting for approval' => 'Awaiting for approval',
+  'Awaiting' => 'Awaiting',
+);
+ ?>
